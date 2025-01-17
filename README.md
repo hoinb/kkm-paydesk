e64bef70b5702e232aacb50886f7628342a245cd
Rainer Maier <junkmail@rainer-maier.de>
2025-01-17 17:38:39 +0100

    added first script

--- README.md
+++ README.md
@@ -2,7 +2,9 @@
 
 ## How to install
 
+```
 $ cd ~
 $ git checkout https://github.com/hoinb/kkm-paydesk.git
 $ cd kkm-paydesk
 $ ./install/install.sh
+```
--- scripts/launch-flohmarkthelfer.sh
+++ scripts/launch-flohmarkthelfer.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-export SECONDHAND_JAR="$(ls -1 $KKM_PAYDESK_BASEDIR/flohmarkthelfer/lib/secondhand-gui-5.*.jar)"
-echo $SECONDHAND_JAR
+SECONDHAND_JAR="$(ls -1 $KKM_PAYDESK_BASEDIR/flohmarkthelfer/lib/secondhand-gui-5.*.jar)"
+echo ">>$SECONDHAND_JAR"
 
 cd $KKM_PAYDESK_BASEDIR/flohmarkthelfer && $JAVA_HOME/bin/java -jar $SECONDHAND_JAR
 cd -
