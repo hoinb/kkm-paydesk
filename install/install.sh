7e83bd3152a5f540600942f0425706055f08dea7
Rainer Maier <junkmail@rainer-maier.de>
2025-01-17 15:51:13 +0100

    added script-server

--- install/install.sh
+++ install/install.sh
@@ -14,6 +14,7 @@ sudo apt update \
 
 sudo apt install -y \
   python3-pip \
+  python3-venv \
   || { echo "apt install failed." ; exit 2; }
 
 # Create Python venv and install required pip packages
@@ -21,7 +22,18 @@ sudo apt install -y \
 # TODO
 
 
-source ./kkm-paydesk.env
+#
+# SECTION 1   ---   Basic Setup
+#
+source ./kkm-paydesk.env \
+  || { echo "cannot source env variables" ; exit 11; }
+
+
+
+
+#
+# SECTION 2  ---   Install Flohmarkthelfer
+#
 
 # Install Java JRE
 rm -rf "$KKM_PAYDESK_BASEDIR/flohmarkthelfer/jre/*"
@@ -33,15 +45,37 @@ elif [ "i386" = "$(dpkg --print-architecture)" ]; then
   l_jreTgzfile=$(ls -1 $KKM_PAYDESK_BASEDIR/install/java-jre/*-linux_i686.tar.gz)
   echo "$l_jreTgzfile"
 else
-  echo "cannot detect system architecture ($(dpkg --print-architecture))" ; exit 5;
+  echo "cannot detect system architecture ($(dpkg --print-architecture))" ; exit 21;
 fi
 
 mkdir -p "$KKM_PAYDESK_BASEDIR/flohmarkthelfer/jre" \
     && tar xzf $l_jreTgzfile -C "$KKM_PAYDESK_BASEDIR/flohmarkthelfer/jre" --strip-components 1 \
-  || { echo "cannot extract Java Runtime Environment" ; exit 6; }
+  || { echo "cannot extract Java Runtime Environment" ; exit 22; }
 
 # Ensure that required folders for Flohmarkthelfer are existing
 mkdir -p "$KKM_PAYDESK_BASEDIR/flohmarkthelfer/data" \
     && mkdir -p "$KKM_PAYDESK_BASEDIR/flohmarkthelfer/log" \
     && mkdir -p "$KKM_PAYDESK_BASEDIR/flohmarkthelfer/sync" \
-  || { echo "cannot create required folders" ; exit 7; }
+  || { echo "cannot create required folders" ; exit 23; }
+  
+  
+
+#
+# SECTION 3   ---   Install Bugy Script Server
+#
+cd $KKM_PAYDESK_BASEDIR \
+  && rm -rf ./python-venv \
+  && mkdir ./python-venv \
+  && python3 -m venv ./python-venv \
+  ||  { echo "cannot install python venv" ; exit 34; }
+./python-venv/bin/pip install \
+    tornado \
+  ||  { echo "cannot install pip packages" ; exit 35; }
+wget "https://github.com/bugy/script-server/releases/download/$KKM_PAYDESK_SCRIPT_SERVER_TAG/script-server.zip" \
+  ||  { echo "cannot download script-server.zip" ; exit 35; }
+rm -rf ./bugy-script-server \
+  && mkdir -p bugy-script-server \
+  && unzip -d bugy-script-server script-server.zip \
+  && rm script-server.zip \
+  ||  { echo "cannot extract script-server.zip" ; exit 35; }
+  
