dbf008217ccbd1f71a74780b6de029ea83ff2da3
Rainer Maier <junkmail@rainer-maier.de>
2025-01-18 10:51:30 +0100

    added config for FMH

--- scripts/launch-flohmarkthelfer.sh
+++ scripts/launch-flohmarkthelfer.sh
@@ -1,9 +1,20 @@
 #!/bin/sh
 
 SECONDHAND_JAR="$(ls -1 $KKM_PAYDESK_BASEDIR/flohmarkthelfer/lib/secondhand-gui-5.*.jar)"
-echo ">>$SECONDHAND_JAR"
+CONFIG_FILE_TEMPLATE="./config.properties.TEMPLATE"
+CONFIG_FILE="./config.properties"
 
-cd $KKM_PAYDESK_BASEDIR/flohmarkthelfer && $JAVA_HOME/bin/java -jar $SECONDHAND_JAR
+cd $KKM_PAYDESK_BASEDIR/flohmarkthelfer
+
+# prepare config file
+rm -f $CONFIG_FILE
+cp $CONFIG_FILE_TEMPLATE $CONFIG_FILE
+sed -i "/buttons.all=/ s/=.*/=$KKM_FLOHMARKTHELFER_BUTTONS_ALL/" $CONFIG_FILE
+sed -i "/peer.name=/ s/=.*/=$HOSTNAME/" $CONFIG_FILE
+
+# lauch Flomarkthelfer
+$JAVA_HOME/bin/java -jar $SECONDHAND_JAR
+
 cd -
 
 
